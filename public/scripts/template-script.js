cc7322ca152abd94eb7944697930979be1e324ad
BrunoSilvaSava <brunossaba.tech@gmail.com>
2021-05-20 00:52:05 -0300

    Initial commit

---
+++ public/scripts/template-script.js
@@ -0,0 +1,14 @@
+const funcionarios = [
+  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
+  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
+  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
+  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
+  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
+  { foto: "", nome: "jose", area: "frontend developer", localidade: "" }
+];
+
+const strutureTable = {
+  
+    funcionarioContainer: document.querySelector('.cards')
+    
+}
