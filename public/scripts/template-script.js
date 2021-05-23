47ff52d3457adb879cdeec6b75d52a0cdab69290
BrunoSilvaSava <brunossaba.tech@gmail.com>
2021-05-23 09:17:44 -0300

    finalizacao front end

--- public/scripts/template-script.js
+++ public/scripts/template-script.js
@@ -1,14 +1,74 @@
 const funcionarios = [
-  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
-  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
-  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
-  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
-  { foto: "", nome: "jose", area: "frontend developer", localidade: "" },
-  { foto: "", nome: "jose", area: "frontend developer", localidade: "" }
+  {
+    id: "0",
+    img: "/imgs/peoples/pexels-justin-shaifer-1222271.jpg",
+    nome: "jose",
+    area: "developer",
+    localidade: "",
+  },
+  {
+    id: "1",
+    img: "/imgs/peoples/pexels-pixabay-371160.jpg",
+    nome: "ana",
+    area: "developer",
+    localidade: "",
+  },
+  {
+    id: "2",
+    img: "/imgs/peoples/pexels-pixabay-371160.jpg",
+    nome: "Dayane",
+    area: "developer",
+    localidade: "",
+  },
+  {
+    id: "3",
+    img: "/imgs/peoples/pexels-pixabay-371160.jpg",
+    nome: "joao",
+    area: "developer",
+    localidade: "",
+  },
+  {
+    id: "4",
+    img: "/imgs/peoples/pexels-pixabay-371160.jpg",
+    nome: "layon",
+    area: "developer",
+    localidade: "",
+  },
+  {
+    id: "5",
+    img: "/imgs/peoples/pexels-pixabay-371160.jpg",
+    nome: "lucas",
+    area: "developer",
+    localidade: "",
+  },
 ];
 
-const strutureTable = {
-  
-    funcionarioContainer: document.querySelector('.cards')
-    
-}
+innerHtmlFuncionarios = () => {
+  var funcionariosTableHtml = document.querySelector(".cards");
+
+  funcionarios.map((val) => {
+    funcionariosTableHtml.innerHTML +=
+      `
+      <div class="card">
+           <span>
+              <img class="column avatar" src="` +
+      val.img +
+      `" alt="avatar">
+                  </span>
+                    <div class="column name-tec">
+                        ` +
+      val.nome +
+      `
+                    </div>
+                    <div class="column area-atuacao">
+                        ` +
+      val.area +
+      `
+                    </div>
+         <img src="https://img.icons8.com/windows/32/000000/like--v1.png" />
+      </div>
+    `;
+  });
+};
+
+innerHtmlFuncionarios();
